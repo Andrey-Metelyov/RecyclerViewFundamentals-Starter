871bbeb43f13032a8b6edf5812c5c4cb4cde10a7
Andrey.Metelyov <andrey.metelyov@gmail.com>
2021-09-23 10:14:52 +0900

    Create binding adapters

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -24,6 +24,11 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(
         }
     }
 
+    fun ViewHolder.bind(item: SleepNight) {
+        binding.sleep = item
+        binding.executePendingBindings()
+    }
+
 //    var data = listOf<SleepNight>()
 //        set(value) {
 //            field = value
@@ -37,26 +42,6 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
         holder.bind(item)
-    }
-
-    fun ViewHolder.bind(item: SleepNight) {
-        val res = itemView.context.resources
-
-        binding.sleepLength.text =
-            convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
-        binding.qualityImage.setImageResource(
-            when (item.sleepQuality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_sleep_active
-            }
-        )
-
     }
 
 //    override fun getItemCount(): Int {
